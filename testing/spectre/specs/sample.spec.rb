03954a40ad3d1c3bd2e6954381b9dde324288605
Christian Neubauer <christian.neubauer@ionos.com>
2021-06-08 14:55:22 +0200

    Spectre tests added

---
+++ testing/spectre/specs/sample.spec.rb
@@ -0,0 +1,20 @@
+describe 'Git' do
+  it 'does some Git operations', tags: [:sample] do
+    git 'example' do
+      branch 'main'
+      clone
+
+      content = read_file('dummy.txt')
+
+      content = content.to_i + 1
+
+      write_file('dummy.txt', content)
+
+      add('dummy.txt')
+      commit('Dummy file updated')
+      push
+
+      cleanup
+    end
+  end
+end
